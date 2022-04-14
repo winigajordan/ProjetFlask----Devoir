a439334e820be12fe422502dd9f115c4b9e4aebb
winigajordan <rwiniga@gmail.com>
2022-04-14 07:09:00 +0000

    Connexion admin

--- controllers/shared_controller.py
+++ controllers/shared_controller.py
@@ -1,7 +1,9 @@
 from models.compte import *
+import hashlib
 
 def admin_login(request):
     login = request.form['login']
     password = request.form['pwd']
-    user = Admin.query.filter_by(login = login, password = password).first()
+    pwd = hashlib.md5(password.encode('utf-8')).hexdigest()
+    user = Admin.query.filter_by(login = login, password = pwd).first()
     return user
--- routes/shared.py
+++ routes/shared.py
@@ -1,14 +1,22 @@
-from flask import render_template, Blueprint, request
+from flask import flash, redirect, render_template, Blueprint, request, session, url_for
 from controllers.shared_controller import admin_login
 
 
 security = Blueprint("security", __name__)
 
 @security.route("/login", methods = ["GET","POST"])
-def admin():
+def admin_log():
     if request.method == 'POST':
         user = admin_login(request)
         if user == None:
-            
+            flash('Login ou mot de passe incorrecte')
+            return render_template("./shared/login.html")
+        else:
+            session["user_name"]=f"{user.nom} {user.prenom}"
+            return redirect(url_for("dash.admin_accueil"))
+    return render_template("./shared/login.html")
 
+@security.route("/logout")
+def logout():
+    session.pop("user_name", None)
     return render_template("./shared/login.html")
